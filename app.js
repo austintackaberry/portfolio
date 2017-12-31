3b5da9448d02212f6d013edc74bff93df844fb12
Austin Tackaberry <austintackaberry@gmail.com>
2017-12-30 23:44:50 -0800

    added app.listen

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ var app = express();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static('public'));
 }
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3001);
+console.log('listening on 3001');
+
+module.exports = app;
